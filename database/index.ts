d1dd9b7052d1201096dfb775760aaa3038a5d326
sshuvoo <safayanshuvo@gmail.com>
2024-04-01 17:07:40 +0600

    Removes a database query

--- database/index.ts
+++ database/index.ts
@@ -4,12 +4,8 @@ const findById = async (_id: string) => {
   return photos.find(({ id }) => id === _id);
 };
 
-const findByIdSync = (_id: string) => {
-  return photos.find(({ id }) => id === _id);
-};
-
 const find = async () => {
   return photos.map(({ id, title, url }) => ({ id, title, url }));
 };
 
-export { photos, findById, find, findByIdSync };
+export { photos, findById, find };
